b19a87a2b8b6328a603daf1f1c01cbc26ec5e882
zabu <ruben.yojay@gmail.com>
2021-05-27 12:20:11 -0400

    added other bootstrap components, finished polishing content details and made indentation and documentation to files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+  // Navbar background color change
+
   var nav = document.querySelector('nav');
 
   window.addEventListener('scroll', function () {
@@ -9,4 +11,28 @@ $(document).ready(function () {
       $(nav).css('background-color', 'initial');
     }
   });
+
+  // Smooth Scroll
+
+  $("a").click(function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function () {
+        window.location.hash = gato;
+      });
+    }
+  });
+
+  // Popovers (enabled everywhere)
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
 })
